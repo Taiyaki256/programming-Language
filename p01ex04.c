44ee527d2ecc2395d040e6eaf526fac8761bf7b8
Taiyaki256 <taiyakey@mineo.jp>
2023-04-17 00:51:53 +0900

    0105

--- p01ex04.c
+++ p01ex04.c
@@ -8,16 +8,21 @@ int main()
 	int x = 17528;
 	int y = 1482;
 
-	printf("%d * %d = %8d\n", x, y, x*y);
-	printf("%d / %d = %8d\n", x, y, x/y);
-	printf("%d + %d = %8d\n", x, y, x+y);
-	printf("%d - %d = %8d\n", x, y, x-y);
-	printf("%d %% %d = %8d\n", x, y, x%y);
+	printf("%d * %d = %8d\n", x, y, x * y);
+	printf("%d / %d = %8d\n", x, y, x / y);
+	printf("%d + %d = %8d\n", x, y, x + y);
+	printf("%d - %d = %8d\n", x, y, x - y);
+	printf("%d %% %d = %8d\n", x, y, x % y);
 
 	return 0;
 }
 
 /*** 結果 ***
 
+17528 * 1482 = 25976496
+17528 / 1482 =       11
+17528 + 1482 =    19010
+17528 - 1482 =    16046
+17528 % 1482 =     1226
 
 *************/
--- p01ex05.c
+++ p01ex05.c
@@ -7,6 +7,7 @@ int main()
 {
 	double temperature; /*温度をあらわす変数[℃]*/
 	double velocity;	/*音速をあらわす変数[m/s]*/
+	printf("temperature=>");
 	scanf("%lf", &temperature);
 	velocity = 331.5 + 0.6 * temperature;
 	printf("temperature=%.1f\n", temperature);
@@ -16,5 +17,20 @@ int main()
 
 /*** 結果 ***
 
+temperature=>15.0
+temperature=15.0
+velocity   =340.5
+
+temperature=>20.0
+temperature=20.0
+velocity   =343.5
+
+temperature=>25.0
+temperature=25.0
+velocity   =346.5
+
+temperature=>30.0
+temperature=30.0
+velocity   =349.5
 
 *************/

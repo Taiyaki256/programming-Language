8a7997bdd9f163d0f5697f577ef5ecb443601427
Taiyaki256 <taiyakey@mineo.jp>
2023-05-08 10:17:54 +0900

    fix

--- p02ex04.c
+++ p02ex04.c
@@ -5,21 +5,21 @@
 
 int main()
 {
-	int x, y1, y2, y3;
-	printf("   x    y1    y2     y3\n");
-	for (x = 1; x <= 100; x++)
-	{
-		y1 = x % 2;
-		y2 = x % 3;
-		y3 = x % 4;
-		printf("%4d %6d %6d %6d\n", x, y1, y2, y3);
-	}
-	return 0;
+   int x, y1, y2, y3;
+   printf("   x     y1     y2     y3\n");
+   for (x = 1; x <= 100; x++)
+   {
+      y1 = x % 2;
+      y2 = x % 3;
+      y3 = x % 4;
+      printf("%4d %6d %6d %6d\n", x, y1, y2, y3);
+   }
+   return 0;
 }
 
 /*** 結果 ***
 
-   x    y1    y2     y3
+   x     y1     y2     y3
    1      1      1      1
    2      0      2      2
    3      1      0      3
